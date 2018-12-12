e78fb39378e5d8479e30f225e619cbd85a21597d
Pedro <pedrlz.frn@gmail.com>
2018-12-12 11:13:49 -0200

    tá funcionando, pelo menos

---
+++ README.md
@@ -0,0 +1,3 @@
+
+A simple project made with express and socket.io based on tutorial from
+https://socket.io/get-started/chat/
--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 
 /* jshint esversion:6*/
 
-const express = require('express');
-const http = require('http');
-
-const app = express();
-const server = http.Server(app);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 8000;
 
-server.listen(PORT, () => {
-  console.log(`Server running at ${PORT}`);
-});
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server running at ${PORT}`);
 });
